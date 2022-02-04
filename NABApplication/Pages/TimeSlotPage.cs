13c22910b231e44da9c1c6e114f915a0189c0209
Dania Joseph <josephd@ROADS.VIC.GOV.AU>
2022-02-05 00:50:45 +1100

    added more wait for time page

--- NABApplication/Pages/TimeSlotPage.cs
+++ NABApplication/Pages/TimeSlotPage.cs
@@ -23,14 +23,14 @@ namespace NABApplication.Pages
         }
         #endregion
         private By _shadowRoot = By.XPath("//*[@id='wrapper']//self-serve-appointment-booking");
-        private By _7thDateOption = By.CssSelector("button[value='10']");
+        private By _7thDateOption = By.CssSelector("button[value='9']");
         private By _firstTimeOption = By.CssSelector("div > div > div > div > div.sc-gtsrHT.Rowstyle__StyledRow-mjf486-0.iHXuUE.cwoAMR.StyledRow-bfNfbx.jlSDJf > div.sc-dlnjwi.Colstyle__StyledCol-sc-1evc4kf-0.ldCTSc.duEYaW.LeftContentCol-fOupiG.SelectTimeSlotCol-kxmyod.fKiecM.dUxGgZ > form > div:nth-child(4) > div > div:nth-child(3) > div > div:nth-child(2) > div:nth-child(1) > button");
         private By _timeSlotFormNextButton = By.CssSelector("button[form='timeslot-form']");
         public void SelectAppointDateAndTime()
         {
-            Thread.Sleep(2000);
-            IWebElement shadowRoot = _javascriptUtil.ExpandRootElement(_elementUtil.GetElement(_shadowRoot));
             Thread.Sleep(3000);
+            IWebElement shadowRoot = _javascriptUtil.ExpandRootElement(_elementUtil.GetElement(_shadowRoot));
+            Thread.Sleep(4000);
             shadowRoot.FindElement(_7thDateOption).Click();
             Thread.Sleep(3000);
             shadowRoot.FindElement(_firstTimeOption).Click();
