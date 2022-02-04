df6b4a6be6c8828d925551a19b81a67b8aa11bca
Dania Joseph <josephd@ROADS.VIC.GOV.AU>
2022-02-05 01:00:17 +1100

    changed the element identifier for appointment time

--- NABApplication/Pages/TimeSlotPage.cs
+++ NABApplication/Pages/TimeSlotPage.cs
@@ -23,7 +23,7 @@ namespace NABApplication.Pages
         }
         #endregion
         private By _shadowRoot = By.XPath("//*[@id='wrapper']//self-serve-appointment-booking");
-        private By _7thDateOption = By.CssSelector("button[value='9']");
+        private By _7thDateOption = By.CssSelector("#date-20220210");
         private By _firstTimeOption = By.CssSelector("div > div > div > div > div.sc-gtsrHT.Rowstyle__StyledRow-mjf486-0.iHXuUE.cwoAMR.StyledRow-bfNfbx.jlSDJf > div.sc-dlnjwi.Colstyle__StyledCol-sc-1evc4kf-0.ldCTSc.duEYaW.LeftContentCol-fOupiG.SelectTimeSlotCol-kxmyod.fKiecM.dUxGgZ > form > div:nth-child(4) > div > div:nth-child(3) > div > div:nth-child(2) > div:nth-child(1) > button");
         private By _timeSlotFormNextButton = By.CssSelector("button[form='timeslot-form']");
         public void SelectAppointDateAndTime()
